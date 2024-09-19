a9c3c127c259e03d18381c77eb6ed7cd055c2bd6
felipetaua <taua.felipee@gmail.com>
2024-09-19 14:06:34 -0300

    Finalizando o front-end

--- recursos/particle.js
+++ recursos/particle.js
@@ -1,57 +1,66 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 6, density: { enable: true, value_area: 800 } },
-    color: { value: "#424242" },
+    number: { value: 24, density: { enable: false, value_area: 10000 } },
+    color: { value: "#ffffff" },
     shape: {
-      type: "polygon",
-      stroke: { width: 0, color: "#000" },
-      polygon: { nb_sides: 6 },
-      image: { src: "img/github.svg", width: 100, height: 100 },
+      type: "star",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 12 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
     },
     opacity: {
-      value: 0.3,
-      random: true,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+      value: 0.03206824121731046,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
     },
     size: {
-      value: 160,
-      random: false,
-      anim: { enable: true, speed: 10, size_min: 40, sync: false },
+      value: 116.2473744127504,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 114.50288693448967,
+        size_min: 0.1,
+        sync: true
+      }
     },
     line_linked: {
       enable: false,
-      distance: 200,
+      distance: 150,
       color: "#ffffff",
-      opacity: 1,
-      width: 2,
+      opacity: 0.4,
+      width: 1
     },
     move: {
       enable: true,
-      speed: 8,
-      direction: "none",
-      random: false,
+      speed: 6,
+      direction: "bottom-right",
+      random: true,
       straight: false,
-      out_mode: "out",
+      out_mode: "bounce",
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-    },
+      attract: {
+        enable: true,
+        rotateX: 1443.0708547789707,
+        rotateY: 1443.0708547789707
+      }
+    }
   },
   interactivity: {
     detect_on: "canvas",
     events: {
       onhover: { enable: false, mode: "grab" },
       onclick: { enable: false, mode: "push" },
-      resize: true,
+      resize: true
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
       repulse: { distance: 200, duration: 0.4 },
       push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
+      remove: { particles_nb: 2 }
+    }
   },
-  retina_detect: true,
+  retina_detect: true
 });
 var count_particles, stats, update;
 stats = new Stats();
