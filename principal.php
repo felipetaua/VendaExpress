aa0222e752170fbbebbebac9f5ffa637fd49b4a9
felipetaua <taua.felipee@gmail.com>
2024-10-18 17:22:58 -0300

    editando as info

--- principal.php
+++ principal.php
@@ -1,6 +1,28 @@
 <?php 
   include "validacao.php";
   include "conexao.php";
+
+  // destino do formulário para inserir por padrão
+  $destino = './usuario/inserir.php';
+
+  // se idAlt for diferente de vazio - se existir idAlt
+  if (!empty($_GET['idAlt'])) {
+    // gurda na variável $id o valor da pessoa clicado no lápis da tabela 
+    $id = $_GET['idAlt'];
+
+    //busca o udsuário do idAlt
+    $sql = "SELECT * FROM usuario WHERE id= '$id' ";
+
+    //executa comando
+    $dados = mysqli_query($conexao, $sql);
+
+    //Váriavel com nossos dados: 
+    $dadosAlt = mysqli_fetch_assoc($dados);
+
+    // echo "Carregado pessoa: ".$dadosAlt['nome'] //testando o funcionamento, ['nomd'] pode ser alterado para outras informações como senha, email, nome;
+
+    $destino = './usuario/alterar.php';
+  }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -67,18 +89,22 @@
                 <div class="row">
                     <div class="col-md card">
                     <h3>Cadastro</h3>
-                          <form action="./usuario/inserir.php" method="post">  
+                          <form action="<?=$destino?>" method="post"> 
+                          <div class="form-group">
+                              <label> ID </label>
+                              <input value="<?php echo isset($dadosAlt) ? $dadosAlt['id'] : '' ?>" name="id" class="form-control" placeholder="Seu id" readonly>
+                            </div> 
                             <div class="form-group">
                               <label> Nome </label>
-                              <input name="nome" type="text" class="form-control" placeholder="Seu nome">
+                              <input value="<?php echo isset($dadosAlt) ? $dadosAlt['nome'] : '' ?>" name="nome" type="text" class="form-control" placeholder="Seu nome" required>
                             </div>
                             <div class="form-group">
                               <label> CPF </label>
-                              <input name="cpf" type="text" class="form-control cpf" placeholder="seu cpf">
+                              <input value="<?php echo isset($dadosAlt) ? $dadosAlt['cpf'] : '' ?>" name="cpf" type="text" class="form-control cpf" placeholder="seu cpf" required>
                             </div>
                             <div class="form-group">
                               <label> Senha </label>
-                              <input name="senha" type="password" class="form-control" placeholder="Senha">
+                              <input value="<?php echo isset($dadosAlt) ? $dadosAlt['senha'] : '' ?>" name="senha" type="password" class="form-control" placeholder="Senha" required>
                             </div>
                             <button type="submit" class="btn btn-success btn-form">Cadastrar</button>
                             <button type="reset" class="btn btn-outline-danger btn-form">Limpar</button>
@@ -113,7 +139,7 @@
                                 <td> <?php echo $colunas['nome'] ?> </td>
                                 <td> <?php echo $colunas['cpf'] ?> </td>
                                 <td> 
-                                  <a href="#"><i class="fa-regular fa-pen-to-square mr-10"></i></a>
+                                  <a href="principal.php?idAlt=<?= $colunas['id'] ?>"><i class="fa-regular fa-pen-to-square mr-10"></i></a>
                                   <a href="<?php echo './usuario/excluir.php?id='.$colunas['id'] ?>"><i class="fa-regular fa-trash-can" style="color: #f21818;"></i></a>
                                </td>
                               </tr>
