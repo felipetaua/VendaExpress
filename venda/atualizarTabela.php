277c3c539c8d3c4dccc6021f5e27250440088749
felipetaua <taua.felipee@gmail.com>
2024-12-10 16:45:01 -0300

    ataualizar tabela

--- venda/atualizarTabela.php
+++ venda/atualizarTabela.php
@@ -4,6 +4,13 @@
 $totalQuantidade = 0;
 $totalValor = 0;
 
-// Buscar os itens de venda da venda atual
+// Buscar os itens_venda/produto da venda atual
 $itensVenda = $conexao->query("SELECT iv.id, p.nome, iv.produto_id, iv.valor FROM item_venda iv JOIN produto p ON iv.produto_id = p.id WHERE iv.venda_id = $idVenda");
+
+// Atualizar os totalizadores 
+while($item = $itensVenda->fetch_assoc()){
+    $totalQuantidade += $item['quantidade']; 
+    $totalValor += $item['quantidade'] * $item['valor']; 
+}
+
 ?>
